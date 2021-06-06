2a1103e69ed77dfeefa338f9b6d2eb120a9bf06d
Jenni Davila <pazjenni1331@gmail.com>
2021-06-06 14:19:02 -0500

    added time to save to local storage, but unable to get text area to show value in local storage

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ var five = timeBlocks[11].firstChild.data;
 
 var selectedBlocks = [six, seven, eight, nine, ten, eleven, twelve, one, two, three, four, five];
 
+var buttons = document.querySelectorAll(".btn"); //save buttons 
+var btnLength = buttons.length; //since generating an array, need to grab length and generate a for loop so event listener can apply to all buttons
 
 var timeEl = document.getElementById("currentDay");
 
@@ -21,7 +23,6 @@ function currentDay() {
     
     var timeInterval = setInterval(function() {
       timeEl.textContent = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-    
     }, 1000);
 };
 
@@ -29,61 +30,77 @@ currentDay();
 
 
 //find a way to grab elements of the time-blocks and create a for loop
-function agendaTasks() {
-  var currentTime = moment().format("h:mm a");
+// function agendaTasks() {
+//   var currentTime = moment().format("h:mm a");
 
-  for(var i=0; i<selectedBlocks.length; i++){
+//   for(var i=0; i<selectedBlocks.length; i++){
   
-    if (currentTime > selectedBlocks[i]) {
-      console.log("Ahead of time!");
-      selectedBlocks [i] = document.querySelector(".hour");
-      selectedBlocks[i].style.backgroundColor = "green";
-    } else if (currentTime < selectedBlocks[i]) {
-      console.log("Running behind!");
-      selectedBlocks[i] = document.querySelector(".hour");
-      selectedBlocks[i].style.backgroundColor = "red";
-    } else if (currentTime === selectedBlocks[i]) {
-      console.log("Just on time!");
-      selectedBlocks[i] = document.querySelector(".hour");
-      selectedBlocks[i].style.backgroundColor = "none";
-    };
+//     if (currentTime > selectedBlocks[i]) {
+//       console.log("Ahead of time!");
+//       selectedBlocks [i] = document.querySelector(".hour");
+//       selectedBlocks[i].style.backgroundColor = "green";
+//     } else if (currentTime < selectedBlocks[i]) {
+//       console.log("Running behind!");
+//       selectedBlocks[i] = document.querySelector(".hour");
+//       selectedBlocks[i].style.backgroundColor = "red";
+//     } else if (currentTime === selectedBlocks[i]) {
+//       console.log("Just on time!");
+//       selectedBlocks[i] = document.querySelector(".hour");
+//       selectedBlocks[i].style.backgroundColor = "none";
+//     };
 
-  };
-};
-agendaTasks();
-
-
-// var buttons = document.querySelectorAll(".btn"); //save buttons 
-// var btnLength = buttons.length; //since generating an array, need to grab length and generate a for loop so event listener can apply to all buttons
-
-// var textInput = document.querySelectorAll(".description").value; //text input into the text area
-
-
-// var storageInput = document.querySelectorAll(".storageInput");
-
-// var storedInput = localStorage.getItem("textInput"); //whatever is saved in local storage 'textInput' the browser will retreive
-
-
-
-
-// function saveToLocalStorage () {
-
-  
-//   localStorage.setItem('textInput', textInput.textContent);
-  
+//   };
 // };
+// agendaTasks();
 
 
 
 
 
 
-// var savedInput = JSON.parse(localStorage.getItem("textInput"));
+
+var storedInput = localStorage.getItem("textInput"); //whatever is saved in local storage 'textInput' the browser will retreive
+
+textInput.addEventListener('input', tasks => {
+  textInput.textContent = tasks.target.value
+  });
 
 
-// for (var i=0; i<btnLength; i++) {
-//   buttons[i].addEventListener('click', saveToLocalStorage);  //eventListener for save buttons -- if multiple save buttons, have to create a for-loop for event listener to apply to each
-// };
+
+
+
+
+var textInput = document.querySelectorAll(".description"); //text input into the text area
+var savedInput = JSON.parse(localStorage.getItem("textInput")) || [];
+
+for (var i=0; i<btnLength; i++) {
+  buttons[i].addEventListener('click', function () {
+   
+    var selectedText = {
+      text: document.querySelectorAll(".description").value,
+      time: timeEl.textContent = moment().format("dddd, MMMM Do YYYY, h:mm:ss a"),
+    }; //objects will be pushed into local storage
+    
+    savedInput.push(selectedText);
+  
+    localStorage.setItem('textInput', JSON.stringify(savedInput));
+    alert("Schedule has been saved");
+    console.log("Schedule saved")
+
+  }, false);
+  };  //eventListener for save buttons -- if multiple save buttons, have to create a for-loop for event listener to apply to each
+
+ 
+ 
+  
+
+
+
+
+
+
+
+
 
 
 
